cec5ea1bd763366c90bf401d81dd2536eb7b484d
Michael Chittenden <mchitten@gmail.com>
2013-07-09 13:30:00 -0400

    Removes unused Mailer.

--- app/mailers/mailer.rb
+++
@@ -1,7 +0,0 @@
-class Mailer < ActionMailer::Base
-  default :from => "Hilary at DoSomething.org <animals@dosomething.org>"
-
-  def signup(email)
-  	mail(:to => email, :subject => "Thanks for signing up for Pics for Pets!")
-  end
-end
