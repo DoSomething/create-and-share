e3199c3605f6790d24f29a5007063fc07aa71f0a
Michael Chittenden <mchitten@gmail.com>
2013-07-12 10:21:45 -0400

    Super basic cleanup stuff.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130711192340) do
+ActiveRecord::Schema.define(:version => 20130711203202) do
 
   create_table "api_keys", :force => true do |t|
     t.string   "key"
@@ -27,9 +27,15 @@ ActiveRecord::Schema.define(:version => 20130711192340) do
     t.string   "lead"
     t.string   "lead_email"
     t.string   "developers"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.boolean  "gated"
+    t.text     "description"
+    t.string   "image"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "posts", :force => true do |t|
