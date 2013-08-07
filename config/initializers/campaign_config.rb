c10efad9450185b2b5120ec2d48161c5ff828475
Marvin Qian <fueledbymarvin@gmail.com>
2013-08-07 15:39:03 -0400

    Setup observer and generator to remove files when a campaign is deleted

--- config/initializers/campaign_config.rb
+++ config/initializers/campaign_config.rb
@@ -1,29 +1,22 @@
-class CampaignSettings
-  cattr_accessor :filters, :facebook, :popups
+filters = {}
+facebook = {}
+popups = {}
+files = Dir["#{Rails.root}/config/campaigns/*.yml"]
+files.each do |file|
+  k = Pathname.new(file).basename.to_s.gsub('.yml', '')
+  filters[k] ||= []
+  facebook[k] ||= []
+  popups[k] ||= []
 
-  @@filters = {}
-  @@facebook = {}
-  @@popups = {}
-  def initialize
-  	files = Dir["#{Rails.root}/config/campaigns/*.yml"]
-  	files.each do |file|
-  	  k = Pathname.new(file).basename.to_s.gsub('.yml', '')
-  	  @@filters[k] ||= []
-      @@facebook[k] ||= []
-      @@popups[k] ||= []
-
-  	  f = YAML::load(File.open(file))
-      @@filters[k] = f['filters'] || {}
-      @@facebook[k] = f['facebook'] || {}
-      @@popups[k] = f['popups'] || {}
-  	end
-  end
+  f = YAML::load(File.open(file))
+  filters[k] = f['filters'] || {}
+  facebook[k] = f['facebook'] || {}
+  popups[k] = f['popups'] || {}
 end
 
-settings = CampaignSettings.new
-CreateAndShare::Application.config.filters = settings.filters
-CreateAndShare::Application.config.facebook = settings.facebook
-CreateAndShare::Application.config.popups = settings.popups
+CreateAndShare::Application.config.filters = filters
+CreateAndShare::Application.config.facebook = facebook
+CreateAndShare::Application.config.popups = popups
 
 if ActiveRecord::Base.connection.table_exists? 'campaigns'
   Campaign.find(:all, :select => 'path').each do |campaign|
