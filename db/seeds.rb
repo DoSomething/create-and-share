85631deb97dadf5a6cf1594fc7a1fbbc161ada9c
Michael Chittenden <mchitten@gmail.com>
2013-08-12 12:21:29 -0400

    Seed Project Lunch data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Campaign.create({
+  title: 'Project Lunch',
+  start_date: '2013-08-12 16:05:00',
+  end_date: '2016-08-12 16:05:00',
+  path: 'lunch',
+  lead: 'Farah',
+  lead_email: 'fshake@dosomething.org',
+  developers: 'mchittenden@dosomething.org, mwatson@dosomething.org',
+  gated: true,
+  description: "Push for better quality lunch food with Project Lunch.  Schools around the nation are serving students inedible junk and we'd like to change that.  Upload a picture your lunch, rate it, and share it with your friends.",
+  image: File.new(Rails.root + 'app/assets/images/campaigns/lunch/sandwich.jpg'),
+  mailchimp: "PicsforPets2013",
+  mobile_commons: "158551",
+  email_submit: "PicsforPets_2013_Reportback",
+  email_signup: "PicsforPets_2013_Reportback",
+  meme_header: "",
+  meme: false
+})
