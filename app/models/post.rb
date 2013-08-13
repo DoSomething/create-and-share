146b5778a1dc373d7519530d7881ed379c3317ac
Michael Chittenden <mchitten@gmail.com>
2013-08-13 11:27:00 -0400

    Fixes filters with no "where" config

--- app/models/post.rb
+++ app/models/post.rb
@@ -152,33 +152,35 @@ class Post < ActiveRecord::Base
     i = 0
     @results = self
 
-    @where.each do |column, value|
-      if cols.include? column
-        if @result.names.length > 0
-          if !@result[value].nil?
-            @results = @results.where(column.to_sym => @result[value])
+    unless @where.nil?
+      @where.each do |column, value|
+        if cols.include? column
+          if @result.names.length > 0
+            if !@result[value].nil?
+              @results = @results.where(column.to_sym => @result[value])
+            end
+          else
+            @results = @results.where(column.to_sym => value)
           end
         else
-          @results = @results.where(column.to_sym => value)
-        end
-      else
-        col_alias = "t#{i.to_s}"
-        if @result.names.length > 0
-          if !@result[value].nil?
-            value = @result[value]
+          col_alias = "t#{i.to_s}"
+          if @result.names.length > 0
+            if !@result[value].nil?
+              value = @result[value]
+            end
+          else
+            value = value
           end
-        else
-          value = value
-        end
 
-        @results = @results
-          .joins('INNER JOIN tags ' + col_alias + ' ON (' + col_alias + '.post_id = posts.id)')
-          .where(col_alias + '.column = ? and ' + col_alias + '.value = ?', column, value)
-        i += 1
+          @results = @results
+            .joins('INNER JOIN tags ' + col_alias + ' ON (' + col_alias + '.post_id = posts.id)')
+            .where(col_alias + '.column = ? and ' + col_alias + '.value = ?', column, value)
+          i += 1
+        end
       end
     end
 
-    if !@order.nil? && @order.count > 0
+    unless @order.nil? || @order.count == 0
       @results = @results.reorder('')
       @order.each do |o|
         @results = @results.order(o)
