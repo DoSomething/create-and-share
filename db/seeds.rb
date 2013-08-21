c9f5b6676e1d24729ae0c2e2a817e2586d60780e
Michael Chittenden <mchitten@gmail.com>
2013-08-21 10:39:49 -0400

    lunch -> fedup

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Campaign.create!({
-  title: 'Project Lunch',
+  title: 'Fed Up',
   start_date: '2013-08-12 16:05:00',
   end_date: '2016-08-12 16:05:00',
-  path: 'lunch',
+  path: 'fedup',
   lead: 'Farah',
-  lead_email: 'fshake@dosomething.org',
+  lead_email: 'lunch@dosomething.org',
   developers: 'mchittenden@dosomething.org, mwatson@dosomething.org',
   gated: 'submit',
-  description: "Push for better quality lunch food with Project Lunch.  Schools around the nation are serving students inedible junk and we'd like to change that.  Upload a picture your lunch, rate it, and share it with your friends.",
+  description: "Push for better quality lunch food with Fed Up.  Schools around the nation are serving students inedible junk and we'd like to change that.  Upload a picture your lunch, rate it, and share it with your friends.",
   image: File.new(Rails.root + 'app/assets/images/campaigns/lunch/sandwich.jpg'),
   mailchimp: "PicsforPets2013",
   mobile_commons: "158551",
