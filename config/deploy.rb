f6e71e24b3143c2a726506db2435f66cf8f58aaa
Michael Chittenden <mchitten@gmail.com>
2013-09-03 15:58:39 -0400

    Try with password?

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :gateway, 'admin.dosomething.org:38383'
 server 'campaigns.dosomething.org', :app, :web, :db
 set :port, '38383'
 set :user, 'dosomething'
+set :password, ENV['DS_DEPLOY_PASS']
 ssh_options[:keys] = [ENV['CAP_PRIVATE_KEY']]
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
