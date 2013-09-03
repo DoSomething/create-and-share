fcc3061431ab69a9c707faa197d29ae9d4894554
Michael Chittenden <mchitten@gmail.com>
2013-09-03 15:37:20 -0400

    I get it now!

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :branch, "lunch"
 
 server 'admin.dosomething.org', :app, :web, :db
 set :port, '38383'
-ssh_options[:keys] = [ENV['DS_DEPLOY_KEY_PRIVATE']]
+ssh_options[:keys] = [ENV['CAP_PRIVATE_KEY']]
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
