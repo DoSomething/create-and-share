024af8756483fa80a799e12ae89e5a4bb55c4e0d
Michael Chittenden <mchitten@gmail.com>
2013-09-04 11:33:05 -0400

    Adds a throbber for autocomplete

--- app/assets/javascripts/campaigns/fedup/submit.js
+++ app/assets/javascripts/campaigns/fedup/submit.js
@@ -11,13 +11,19 @@ $(document).ready(function() {
         },
         success: function(data) {
           response(data);
-        },
+        }
       });
+    },
+    change: function(event, ui) {
+      $('.ui-autocomplete-input').addClass('throbbing');
+    },
+    close: function() {
+      $('.ui-autocomplete-input').removeClass('throbbing');
     },
     minLength: 2,
     select: function( event, ui ) {
       // Select logic here
-    },
+    }
   });
 
   if ($('#post_state').val() == "") {
