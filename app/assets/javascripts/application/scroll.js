47ba9f41ca0f01bd2668e9067c5041e061949e47
Michael Chittenden <mchitten@gmail.com>
2013-09-13 11:22:56 -0400

    Leaves perception of faster infinite scroll

--- app/assets/javascripts/application/scroll.js
+++ app/assets/javascripts/application/scroll.js
@@ -2,45 +2,54 @@ $(document).ready(function() {
   var page = 0;     // Default page
   var running = 10; // How many posts are currently shown
   var done = [];
-  var returned = 0;
+  var loaded = [];
 
   function in_view() {
-    $('.inview').bind('inview', function(event, visible) {
-      if (count <= 10) {
-        $('.inview').remove();
-        return false;
-      }
-
+    // Start the scroll load on the last post.  Leaves the perception of a faster infinite scroll.
+    $('.id-' + latest).bind('inview', function(event, visible) {
       // Page + 1
       page++;
+      var $elm = $(this);
 
+      // Load the next scroll
       $.post('/' + campaign.path + '/posts/scroll.json', { filter: filter, type: campaign.scroll_type, page: page, last: latest }, function(response) {
         returned = response.returned;
         latest = response.latest;
 
-        $(response.posts).appendTo($('.post-list'));
-        $('img.lazy').lazyload();
+        // Bind the "inview" element to show more posts, if applicable.
+        $('.inview').bind('inview', function(event, visible) {
+          if (count <= 10) {
+            $('.inview').remove();
+            return false;
+          }
 
-        // Remove the current inview element.  Add a new one.
-        $('.inview').remove();
-        if (response.die) {
-          return false;
-        }
+          // Unbind the last, last post's ID so we don't load again and again.
+          $elm.unbind('inview');
 
-        $('<div></div>').addClass('inview').appendTo($('.post-list'));
+          $(response.posts).appendTo($('.post-list'));
+          $('img.lazy').lazyload();
 
-        // Load Facebook
-        load_facebook();
-        set_votes();
-        // Running count += returned count
-        running += returned;
-        // Only keep going if there are more posts to show.
-        if (running < count) {
-          in_view();
-        }
-        else {
+          // Remove the current inview element.  Add a new one.
           $('.inview').remove();
-        }
+          if (response.die) {
+            return false;
+          }
+
+          $('<div></div>').addClass('inview').appendTo($('.post-list'));
+
+          // Load Facebook
+          load_facebook();
+          set_votes();
+          // Running count += returned count
+          running += returned;
+          // Only keep going if there are more posts to show.
+          if (running < count) {
+            in_view();
+          }
+          else {
+            $('.inview').remove();
+          }
+        });
       });
     });
   }
